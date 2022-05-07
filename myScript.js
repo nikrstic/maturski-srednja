0239b47baa1019c6f4b820c1c8f3e061925b2ab3
nikrstic <nikola.krstic2@pmf.edu.rs>
2022-05-07 12:04:39 +0200

    commit

---
+++ myScript.js
@@ -0,0 +1,8 @@
+AOS.init();
+AOS.init({
+    duration: 1200
+   });
+let nekoImeVarijable = document.querySelector('#nav_bar');
+    nekoImeVarijable.addEventListener('click', ()=>{
+    nekoImeVarijable.classList.add('aktivna');
+})
